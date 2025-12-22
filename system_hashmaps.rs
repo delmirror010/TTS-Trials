7c5a6fd008156663895311acdd4892436f4bc31e
delmirror010 <delmirror010@gmail.com>
2025-12-21 21:56:23 -0500

    Update system_hashmaps.rs

--- system_hashmaps.rs
+++ system_hashmaps.rs
@@ -1,5 +1,15 @@
 use std::collections::HashMap;
 
-let mut del = HashMap::new();
-del.insert(String::from("-dl"), "Del");
-del.insert(String::from("-dvn"), "Davina");
+let mut did_sys = HashMap::new();
+// check message author what variable type
+did_sys.insert(String::from("id?"), a_sys);
+
+let mut a_sys = HashMap::new();
+a_sys.insert(String::from("-y"), "Yuuen");
+
+pub fn get_member_nickname(shorthand: &str, msg_author: &str) {
+
+  
+  
+}
+
