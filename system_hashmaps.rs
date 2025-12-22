6dbda0578ebf1da078fb3ac9e1aabd59a31996af
delmirror010 <delmirror010@gmail.com>
2025-12-21 22:14:57 -0500

    Update system_hashmaps.rs

--- system_hashmaps.rs
+++ system_hashmaps.rs
@@ -1,15 +1,15 @@
 use std::collections::HashMap;
 
-let mut did_sys = HashMap::new();
-// check message author what variable type
-did_sys.insert(String::from("id?"), a_sys);
+let mut role_sys = HashMap::new();
+did_sys.insert(String::from(1415600828076785705), a_sys);
 
 let mut a_sys = HashMap::new();
 a_sys.insert(String::from("-y"), "Yuuen");
 
-pub fn get_member_nickname(shorthand: &str, msg_author: &str) {
-
+// gets member name from message author role ID and suffix
+pub fn get_member_nickname(shorthand: &str, msg_author_role: i32) {
   
+  return (did_sys.get(msg_author_role)).get(shorthand);  
   
 }
 
